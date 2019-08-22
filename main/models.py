ba0bd9cd9ec3d3656ef4da56548e1bd20a999f83
silenthunter007 <avishek.das.ayan@gmail.com>
2019-08-22 17:46:06 +0600

    5:46

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+	first_name = models.CharField(max_length = 20)
+	last_name = models.CharField(max_length = 20)
+	b_date = models.DateTimeField('Birth Date')
+	roll = models.CharField(max_length = 20)
+	dept = models.CharField(max_length = 20)
+	session  = models.CharField(max_length = 20)
+
+
+class teacher(models.Model):
+	name = models.CharField(max_length = 20)
+	dept = models.CharField(max_length = 20)
+	subject = models.CharField(max_length = 20)
+	
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
+
+
 # Create your views here.
+def homepage(request):
+	return render(
+			request,
+			'main/home.html'
+		)
