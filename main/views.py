01581e74675053251eb5cbc64162b6c1e092f433
silenthunter007 <avishek.das.ayan@gmail.com>
2019-08-23 23:37:17 +0600

    11:37

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as dj_login, logout, authenticate
+from django.contrib import messages
 
 
 
@@ -8,4 +10,38 @@ def homepage(request):
 	return render(
 			request,
 			'main/home.html'
+		)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                dj_login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+
+    return render(request = request,
+                    template_name = "main/login.html",
+                    context={"form":form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("main:homepage")
+
+
+def cpanel(request):
+	return render(
+		request,
+		"main/cpanel.html"
 		)
