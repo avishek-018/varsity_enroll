d69a806827b00b052bf350555356ff8ef080887c
Fatima <fatimajahra1601@gmail.com>
2019-08-26 12:06:32 +0600

    added view_students feature

--- main/urls.py
+++ main/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("logout/", views.logout_request, name="logout"),
     path("cpanel/", views.cpanel, name="cpanel"),
-    
+    path("add_student/", views.add_student, name="add_student"),
+    path("view_students/", views.view_students, name="add_student"),
  ]
